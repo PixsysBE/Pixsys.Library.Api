e64a4417618180d6932608d1fc27e0547e10255d
Sylvain Signore <sylvain.signore@pixsys.be>
2026-06-08 14:50:13 +0200

    feat: key validator

---
+++ src/Pixsys.Library.Api/Pixsys.Library.Api/Validators/ApiKeyValidator.cs
@@ -0,0 +1,14 @@
+﻿using Pixsys.Library.Api.Interfaces;
+
+namespace Pixsys.Library.Api.Validators
+{
+    /// <inheritdoc/>
+    public class ApiKeyValidator : IApiKeyValidator
+    {
+        /// <inheritdoc/>
+        public bool IsValid(string apiKey)
+        {
+            return !string.IsNullOrWhiteSpace(apiKey);
+        }
+    }
+}
