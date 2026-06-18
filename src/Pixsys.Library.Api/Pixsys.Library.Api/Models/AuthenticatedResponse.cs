12292c90ef4ea1bf2965954b4bc9ee6e62a22166
Sylvain Signore <sylvain.signore@pixsys.be>
2026-06-18 12:10:53 +0200

    feat: test cake release

---
+++ src/Pixsys.Library.Api/Pixsys.Library.Api/Models/AuthenticatedResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace Pixsys.Library.Api.Models
+{
+    /// <summary>
+    /// The authenticated response model.
+    /// </summary>
+    public class AuthenticatedResponse
+    {
+        /// <summary>
+        /// Gets or sets the token.
+        /// </summary>
+        public string? Token { get; set; }
+
+        /// <summary>
+        /// Gets or sets the refresh token.
+        /// </summary>
+        public string? RefreshToken { get; set; }
+    }
+}
