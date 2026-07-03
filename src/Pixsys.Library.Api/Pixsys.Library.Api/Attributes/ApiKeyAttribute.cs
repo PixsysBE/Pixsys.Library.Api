3a17b70bb952f32dfa5fd0958ec15bf34df66f2c
Sylvain Signore <sylvain.signore@pixsys.be>
2026-07-03 10:18:28 +0200

    fix: ApiKeyAttribute

---
+++ src/Pixsys.Library.Api/Pixsys.Library.Api/Attributes/ApiKeyAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pixsys.Library.Api.Filters;
+
+namespace Pixsys.Library.Api.Attributes
+{
+    /// <summary>
+    /// Specifies that the ApiKey is required to perform this action.
+    /// </summary>
+    /// <seealso cref="ServiceFilterAttribute" />
+    public class ApiKeyAttribute : ServiceFilterAttribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApiKeyAttribute"/> class.
+        /// </summary>
+        public ApiKeyAttribute()
+            : base(typeof(ApiKeyAuthorizationFilter))
+        {
+        }
+    }
+}
